1f13e021fd4622a5e9eead3aa9b6409af70e205a
RaisanJMR <raisanjmr@protonmail.com>
2021-05-26 23:33:19 +0530

    working on message field

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
 import React, { useState, useEffect } from 'react';
-// import { Button } from '@material-ui/core';
+import FlipMove from 'react-flip-move';
 import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 import './App.css';
 import Message from './Message';
+import firebase from 'firebase';
+import db from './firebase';
+import Logo from './logo.svg';
+import SendIcon from '@material-ui/icons/Send';
+import { IconButton } from '@material-ui/core';
 
 function App() {
   const [input, setInput] = useState('');
   const [messages, setMessages] = useState([
-    { username: 'sonny', text: 'hey guys' },
-    { username: 'raisan', text: 'hello guys' },
-    { username: 'qazi', text: 'hoi!! boys' },
+    // {id: Math.floor(Math.random() * 100), username: 'sonny', message: 'hey guys' },
+    // {id: Math.floor(Math.random() * 100), username: 'raisan', message: 'hello guys' },
+    // {id: Math.floor(Math.random() * 100), username: 'qazi', message: 'hoi!! boys' },
   ]);
   const [username, setUsername] = useState('');
-  console.log(username);
+  // console.log(messages);
+  useEffect(() => {
+    db.collection('messages')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot((snapshot) => {
+        setMessages(
+          snapshot.docs.map((doc) => ({ id: doc.id, message: doc.data() }))
+        );
+      });
+  }, []);
+
   useEffect(() => {
     setUsername(prompt('please enter your name'));
   }, []);
 
   const sendMessage = (e) => {
     e.preventDefault();
-    setMessages([...messages, { username: username, text: input }]);
+    db.collection('messages').add({
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
     setInput('');
   };
 
   return (
     <div className="App">
+      <img src={Logo} alt="logo" width="100" height="100"/>
       <h1>Facebook messenger Clone with reactJS 🚀🚀 firebase</h1>
       <h2>Welcome {username}</h2>
-      <form>
+      <form className="app__form">
         <FormControl>
           <InputLabel>Enter message</InputLabel>
           <Input
@@ -37,20 +57,27 @@ function App() {
               setInput(target.value);
             }}
           />
-          <Button
+
+ 
+
+          <IconButton
             disabled={!input}
             variant="contained"
             color="primary"
             type="submit"
             onClick={sendMessage}
           >
-            Send Message
-          </Button>
+            <SendIcon/>
+          </IconButton>
         </FormControl>
       </form>
-      {messages.map((message, index) => (
-        <Message username={username} message={message} key={index} />
-      ))}
+      {/* REACT FLIPMOVE */}
+      <FlipMove>
+        {messages.map(({id, message}) => (
+          <Message key={id} username={username} message={message} />
+        ))}
+      </FlipMove>
+
     </div>
   );
 }
--- src/Message.js
+++ src/Message.js
@@ -1,26 +1,26 @@
+import React,{forwardRef} from 'react';
 import { Card, CardContent, Typography } from '@material-ui/core';
-import React from 'react';
 import './Message.css';
 
-function Message({ username, message, key }) {
-  // console.log('message->',message);
+const Message = forwardRef (({ username, message },ref)=> {
+  // console.log('message->',message.id);
   // console.log('username->',username);
-  // console.log(key);
+  
   const isUser = username === message.username;
   return (
-    <div className={`message ${isUser && 'message__user'}`}>
+    <div ref={ref} className={`message ${isUser && 'message__user'}`}>
 
     <Card className={isUser? "message__userCard":"message__guestCard"}>
       <CardContent>
         <Typography color="white" variant="h5" component="h2">
         
-          <p key={key}> {message.username} : {message.text}</p> 
+          <p key={message.id}> {message.username} : {message.message}</p> 
         
         </Typography>
       </CardContent>
     </Card>
     </div>
   );
-}
+})
 
 export default Message;
